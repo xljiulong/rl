7ef28ff9e0667bf02653da515a1a26d71e6eb66e
zhangjl19 <zhangjl19@spdb.com.cn>
2024-01-06 14:48:06 +0800

    add beautiful print

--- src/s4.1.state_value_de_opt.py
+++ src/s4.1.state_value_de_opt.py
@@ -25,7 +25,7 @@ class StateValueSimulate(GridWordEnv):
         return state
 
 
-    def step_from_state(self, c_state:int, action: int):
+    def step_from_state(self, c_state:int, action: int, skip_type=False):
         assert self.action_space.contains(action), f'{action} ({type(action)} invalid)'
         old_x, old_y = self._state_to_xy(c_state)
         new_x, new_y = old_x, old_y
@@ -47,9 +47,10 @@ class StateValueSimulate(GridWordEnv):
             new_x = self.n_width - 1
         if new_y >= self.n_height:
             new_y = self.n_height - 1
-        
-        if self.grids.get_type(new_x, new_y) == 1:
-            new_x, new_y = old_x, old_y
+
+        if not skip_type:
+            if self.grids.get_type(new_x, new_y) == 1:
+                new_x, new_y = old_x, old_y
 
         state = self._xy_to_state((new_x, new_y))
        
@@ -61,7 +62,7 @@ class StateValueSimulate(GridWordEnv):
         # TODO 验证获取全集 
         # self.action_space
         for a in range(0, self.action_len): # 可以验证下 action space 如何取全集
-            nxt_state = self.step_from_state(c_state=c_state, action=a)
+            nxt_state = self.step_from_state(c_state=c_state, action=a, skip_type=True)
             if c_state == nxt_state:
                 continue
             successors.append(nxt_state)
@@ -74,13 +75,12 @@ class StateValueSimulate(GridWordEnv):
 
         for s in range(self.n_width * self.n_height):
             for a in range(self.action_len):
-                if s == self.step_from_state(s, a):
+                if s == self.step_from_state(s, a, skip_type=True):
                     continue
-                action = a
                 successor_num = len(self.get_successors(s))
                 self.pi[s][a] = 1.0/successor_num
 
-    def eval_policy(self, theta = 0.0001, eval_round=1000):
+    def eval_policy(self, theta = 0.001, eval_round=100):
         V = np.zeros(self.n_width * self.n_height)
         gamma = 0.8
 
@@ -93,27 +93,40 @@ class StateValueSimulate(GridWordEnv):
                     print(f'state {state} will not get evaled')
                     continue
 
+                if self.grids.get_reward(state) > 0:
+                    continue
+
                 # 迭代
                 for action in range(0, self.action_len):
-                    nxt_sate = self.step_from_state(state, action)
-                    if nxt_sate == state:
-                        continue
+                    nxt_sate = self.step_from_state(state, action, skip_type=True)
+                    # if nxt_sate == state:
+                    #     continue
 
                     reward = self.get_reward(nxt_sate)
 
                     if self.grids.get_type(nxt_sate) == 1:
-                        # V[state] += self.pi[state][action] * (reward + gamma * V[state])
+                        V[state] += self.pi[state][action] * (reward + gamma * V[state])
                         pass
                     else:
                         V[state] += self.pi[state][action] * (reward + gamma * V[nxt_sate])
                     
             delta = max(delta, np.abs(v - V[state]))
             print(f'iter {iter} delta is {delta}')
+            value = np.array(V).reshape(self.n_width, self.n_height)
+            print(f'value is::')
+            self.human_view(V)
+            
 
         if delta <= theta:
             print(f'delta is good enough {delta}')
             exit(0)
 
+    def human_view(self, vec):
+        t_sque = [[n * self.n_width + e for e in range(0, self.n_width)] for n in range(self.n_height - 1, -1, -1)]
+        for line_lst in t_sque:
+            line_lst = [np.round(vec[item], decimals=3) for item in line_lst]
+            line = ' '.join(list(map(str, line_lst)))
+            print(line)
 
 if __name__ == '__main__':
     ss = StateValueSimulate()
