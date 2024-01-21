3bd5e20fd5d08480bc1194176bd129cac5a29d29
zhangjl19 <zhangjl19@spdb.com.cn>
2024-01-21 08:27:39 +0800

    update done

--- src/s8_DynaQ.py
+++ src/s8_DynaQ.py
@@ -1,8 +1,9 @@
 import numpy as np
+import random
 from collections import defaultdict
 from  roomba_env import GridWordEnv
 
-class Sara(GridWordEnv):
+class DynaQ(GridWordEnv):
     def __init__(self, n_width: int = 5, 
                  n_height: int = 5, 
                  u_size=40, 
@@ -109,8 +110,54 @@ class Sara(GridWordEnv):
             line = ' '.join(list(map(str, line_lst)))
             print(line)
 
+
+class Agent():
+    def __init__(self, env) -> None:
+        self.episode = 1
+        self.Q = {}
+        self.model = {}
+        self.actions = [0, 1, 2, 3]
+        self.position = env.start
+
+    def make_model(self, pos, act, reward, next_state):
+        key = (pos, act)
+        value = (reward, next_state)
+        self.model[key] = value
+
+    def get_maxa_from_state(self, state):
+        max_q = -1000
+        ret_act = -1
+        for act in self.actions:
+            a_q = self.get_q(state, act)
+            if a_q >= max_q:
+                ret_act = act
+
+        return ret_act
+
+    def get_q(self, pos, act):
+        key = (pos, act)
+        return self.Q.setdefault(key, format(random.random() / 10000), '.3f')
+    
+    def update_q(self, pos, action, next_pos, reward, alpha=0.1, gamma=0.9):
+        old_q = self.get_q(pos, action)
+        nxt_acion = self.get_maxa_from_state(next_pos)
+        nxt_q = self.get_q((next_pos, nxt_acion))
+        q = old_q + alpha*(reward + gamma * nxt_q - old_q)
+        self[(pos, action)] = float(format(q, '.3f'))
+
+    def q_plan(self, n):
+        for i in range(n):
+            # a = [k for k in self.model.keys()]
+            a = self.model.keys()
+            done = False
+
+            if a != []:
+                pos, act = random.choice(a)
+                reward, next_state = self.model[(pos, act)]
+
+    
 if __name__ == '__main__':
-    ss = Sara()
+    ss = DynaQ()
     alpha = 0.05
     gamma = 0.8
     epsilon = 0.5
